0e02afc878c517bcdbae6472e369f60af79a84d5
lucasr0609 <lucasram2583@gmail.com>
2022-06-29 03:16:15 -0400

    adding password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,32 @@
-// Assignment code here
+let generatePassword = function  () {
+  // Assignment code here
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var symbols = "!#$%&*+-.:;<=>?@^_{|}~";
+  var numbers = "123456789";
+  var passwords = [];
+  //alert the user of criteria
+  window.alert('Please choose password criteria.')
+
+  //set criteria for password 
+  var passwordLength = parseInt(window.prompt('Please choose a length no less than 8 and no more than 128 characters long'));
+  if(passwordLength < 8 || passwordLength > 128 || passwordLength === null || passwordLength === "") {
+    window.alert('Please enter a valid password length.')
+    return ''
+  }
+
+  
+  // store the window object into variable
+  var upperCaseConfirm = window.confirm('promt here')
+ // if the user confirms uppercase letters, push it to the passwords array. 
+ if(upperCaseConfirm) {
+  passwords += upperCase
+ } else {
+    return ''
+ }
+
+
+}
 
 
 // Get references to the #generate element
