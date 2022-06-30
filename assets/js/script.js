d37c2148ae0d307fa9c9292ca0bce4979348bc28
lucasr0609 <lucasram2583@gmail.com>
2022-06-30 00:45:07 -0400

    finished password generator project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,59 @@
-let generatePassword = function  () {
   // Assignment code here
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var symbols = "!#$%&*+-.:;<=>?@^_{|}~";
-  var numbers = "123456789";
-  var passwords = [];
-  //alert the user of criteria
-  window.alert('Please choose password criteria.')
+  var passwordLength = 8;
+  var passwordArr = [];
+  var upperCaseArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var lowerCaseArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var symbolsArr = ["!","#","$","%","&","*","+","-",".",":",";","<","=",">","?","@","^","_","{","|","}","~"];
+  var numbersArr = ["0","1","2","3","4","5","6","7","8","9"];
 
-  //set criteria for password 
-  var passwordLength = parseInt(window.prompt('Please choose a length no less than 8 and no more than 128 characters long'));
-  if(passwordLength < 8 || passwordLength > 128 || passwordLength === null || passwordLength === "") {
-    window.alert('Please enter a valid password length.')
-    return ''
+ function generatePassword() {
+  var password = "";
+  for(var i = 0; i < passwordLength; i++) {
+    var randonIndex = Math.floor(Math.random() * passwordArr.length)
+    password = password + passwordArr[randonIndex];
   }
-
-  
-  // store the window object into variable
-  var upperCaseConfirm = window.confirm('promt here')
- // if the user confirms uppercase letters, push it to the passwords array. 
- if(upperCaseConfirm) {
-  passwords += upperCase
- } else {
-    return ''
- }
-
-
+  return password;
 }
-
+  // store the window object into variable
+  // if the user confirms uppercase letters, push it to the passwords array. 
+  function getPrompts(){
+    passwordArr = [];
+    window.alert('Please choose password criteria.')
+    passwordLength = parseInt(window.prompt('Please choose a length no less than 8 and no more than 128 characters long'));
+    if(passwordLength < 8 || passwordLength > 128 || passwordLength === null || passwordLength === "") {
+      window.alert('Please enter a valid password length.')
+      return ''
+    };
+    if (confirm('Would you like to add upper case letters to your randomize password, if so please press ok if not press cancel')){
+      passwordArr = passwordArr.concat(upperCaseArr);
+    }
+    if (confirm('Would you like to add lower case letters to your randomize password, if so please press ok if not press cancel')){
+      passwordArr = passwordArr.concat(lowerCaseArr);
+    }
+    if (confirm('Would you like to add symbol characters like ! # $ and others to your randomize password, if so please press ok if not press cancel')){
+      passwordArr = passwordArr.concat(symbolsArr);
+    }
+    if (confirm('Would you like to add numbers to your randomize password, if so please press ok if not press cance')){
+      passwordArr = passwordArr.concat(numbersArr);
+    }
+    return true;
+  };
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var rightPrompts = getPrompts();
+  if(rightPrompts){
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
+  }
+  
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// hello world!
